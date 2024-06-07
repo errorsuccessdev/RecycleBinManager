fd8e123169878fba0bd44867365240d00a8f9c23
ann <159976396+annlikeswindows@users.noreply.github.com>
2024-06-07 14:57:24 -0500

    Added license info

--- RBMLauncher/launcher.c
+++ RBMLauncher/launcher.c
@@ -7,6 +7,21 @@
 * program to create a shortcut to that will run silently and launch RBM. 
 * This also allows us to set a static icon in the start menu in a manner that
 * does not interfere with the dynamic icons in RBM.
+* 
+* Copyright (C) 2024 ERROR_SUCCESS Software
+* 
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 #define _CRT_SECURE_NO_WARNINGS
